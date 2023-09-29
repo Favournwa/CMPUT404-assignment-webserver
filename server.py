1abf1701d8ecf64da54455230c53610000b121a3
Favournwa <nwachukw@ualberta.ca>
2023-09-29 04:39:48 -0600

    work on 404 but added comments

--- server.py
+++ server.py
@@ -2,27 +2,28 @@
 import socketserver
 
 
-# Copyright 2013 Abram Hindle, Eddie Antonio Santos
-#
+# Copyright 2023 Favour Nwachukwu, Abram Hindle, Eddie Antonio Santos
+
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-#
+
+# This code was made for CMPUT 404 - Fall 2023 - Assignment 1
+
 # Furthermore it is derived from the Python documentation examples thus
 # some of the code is Copyright © 2001-2013 Python Software
 # Foundation; All Rights Reserved
 #
 # http://docs.python.org/2/library/socketserver.html
-#
+
 # run: python freetests.py
 
 # try: curl -v -X GET http://127.0.0.1:8080/
@@ -31,14 +32,14 @@ import socketserver
 class MyWebServer(socketserver.BaseRequestHandler):
 
     def handle(self):
-        self.data = self.request.recv(1024).strip()
+        self.data = self.request.recv(1024).strip()  # information from the client including http methods and paths.
 
         print("Got a request of: %s\n" % self.data)
 
         http_method = self.data.decode('utf-8').split()[0]
         http_path = self.data.decode('utf-8').split()[1]
 
-        if http_method != "GET":
+        if http_method != "GET":  # if method is not GET (i.e POST, PUT, DELETE)
             response = "HTTP/1.1 405 Method Not Allowed\r\n\r\n".encode()
         else:
             if http_path.endswith("/"):  # if a directory/html file
@@ -56,12 +57,12 @@ class MyWebServer(socketserver.BaseRequestHandler):
                 with open(http_path, 'r') as f:
                     html_file = f.read()
                 response = f"HTTP/1.1 200 OK \r\nContent-Type: text/html\r\n\r\n{html_file}".encode()
-            else:
+            else:  # if a directory without the final slash or not a path
                 try:
                     path = http_path + "/"
                     response = f"HTTP/1.1 301 Moved Permanently \r\nLocation: {path}\r\n\r\n".encode()
                 except FileNotFoundError:
-                    response = "HTTP/1.1 404 Not Found \r\n\r\n<html><h1>404 File not found</h1></html>\r\n\r\n".encode()
+                    response = "HTTP/1.1 404 Not Found \r\nContent-Type: text/html\r\n\r\n<html><h1>404 File not found</h1></html>\r\n\r\n".encode()
 
         self.request.sendall(response)
 
