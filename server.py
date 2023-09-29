ac1cd9e4ecb229739379d6b4b0b20d4ff4fd9ebb
Favournwa <nwachukw@ualberta.ca>
2023-09-29 04:14:23 -0600

    remaining 404 status code

--- server.py
+++ server.py
@@ -1,14 +1,15 @@
-#  coding: utf-8 
+#  coding: utf-8
 import socketserver
 
+
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -28,11 +29,42 @@ import socketserver
 
 
 class MyWebServer(socketserver.BaseRequestHandler):
-    
+
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+
+        print("Got a request of: %s\n" % self.data)
+
+        http_method = self.data.decode('utf-8').split()[0]
+        http_path = self.data.decode('utf-8').split()[1]
+
+        if http_method != "GET":
+            response = "HTTP/1.1 405 Method Not Allowed\r\n\r\n".encode()
+        else:
+            if http_path.endswith("/"):  # if a directory/html file
+                http_path = "./www" + http_path + "index.html"
+                with open(http_path, 'r') as f:
+                    html_file = f.read()
+                response = f"HTTP/1.1 200 OK \r\nContent-Type: text/html\r\n\r\n<body>{html_file}</body>".encode()
+            elif http_path.endswith(".css"):  # if a css file
+                http_path = "./www" + http_path
+                with open(http_path, 'r') as f:
+                    css_file = f.read()
+                response = f"HTTP/1.1 200 OK \r\nContent-Type: text/css\r\n\r\n{css_file}".encode()
+            elif http_path.endswith(".html"):  # if a html file
+                http_path = "./www" + http_path
+                with open(http_path, 'r') as f:
+                    html_file = f.read()
+                response = f"HTTP/1.1 200 OK \r\nContent-Type: text/html\r\n\r\n{html_file}".encode()
+            else:
+                try:
+                    path = http_path + "/"
+                    response = f"HTTP/1.1 301 Moved Permanently \r\nLocation: {path}\r\n\r\n".encode()
+                except FileNotFoundError:
+                    response = "HTTP/1.1 404 Not Found \r\n\r\n<html><h1>404 File not found</h1></html>\r\n\r\n".encode()
+
+        self.request.sendall(response)
+
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
@@ -44,3 +76,5 @@ if __name__ == "__main__":
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
     server.serve_forever()
+
+    connection.close()
